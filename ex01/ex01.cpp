d89df13b40ee308aab399d5161bf43685d0b93c3
gypark <gghotted@gmail.com>
2021-05-05 18:33:23 +0900

    Add ex01

---
+++ ex01/ex01.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+void memoryLeak(void) {
+  std::string *panther = new std::string("String panther");
+  std::cout << *panther << std::endl;
+  delete panther;
+}
+
+int main(void) {
+  memoryLeak();
+  system("leaks a.out");
+}
